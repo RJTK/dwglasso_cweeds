1df2c7f87f6a5e642400bfcb7f100a41ba2cb94e
Ryan J. Kinnear <Ryan@Kinnear.ca>
2017-05-10 14:18:17 -0400

    Added cross validation with differential evolution

--- src/models/dwglasso.py
+++ src/models/dwglasso.py
@@ -7,12 +7,15 @@ NOTE: This file is intended to be executed by make from the top
 level of the project directory hierarchy.  We rely on os.getcwd()
 and it will not work if run directly as a script from this directory.
 '''
+import time
 import sys
 import numba  # JIT compilation
 import numpy as np
 import warnings
 from scipy.linalg import cho_factor, cho_solve, lu_factor, lu_solve, eigh
-from itertools import product
+import matplotlib as mpl; mpl.use('TkAgg')
+from matplotlib import pyplot as plt
+from scipy.optimize import differential_evolution
 from src.conf import MAX_P, ZZT_FILE_PREFIX, YZT_FILE_PREFIX,\
     X_VALIDATE_FILE_PREFIX
 
@@ -48,38 +51,69 @@ def build_YZ(X, p):
     return Y, Z
 
 
-def cross_validate(ZZT: np.array, YZT: np.array, Y: np.array, Z: np.array,
-                   p: int, lmbda: list, alpha: list, delta:
-                   list, sigma: list, mu: float=0.1, tol=1e-6,
+def cross_validate(ZZT: np.array, YZT: np.array, X_test,
+                   p: int, mu: float=0.1, tol=1e-6,
                    max_iter=100, warn_PSD=False, ret_B_err=False,
-                   n_thread: int=8):
+                   t_limit_sec=3600):
     '''
     Run through each possible combinations of parameters given by
     the lists lmbda, alpha, delta, and sigma and then fit the dwglasso
     model and cross validate it against the 1-step ahead prediction
     task on the data given in Z and Y.  Y_hat = B_hat * Z.
     '''
-    n = Y.shape[0]
-    T = Y.shape[1]
-    p = Z.shape[0] // n
-    err_min = np.linalg.norm(Y, ord='fro')**2
-    B_opt = np.zeros((n, n * p))
-    for l, a, d, s in product(lmbda, alpha, delta, sigma):
+    t0 = time.time()
+
+    def tlimit_func(*args, **kwargs):
+        if time.time() - t0 >= t_limit_sec:
+            return True
+        return
+
+    def f(x):
+        l, a, d, s = x
         B_hat = dwglasso(ZZT=ZZT, YZT=YZT, p=p, lmbda=l, alpha=a,
                          mu=mu, delta=d, sigma=s, tol=tol,
-                         warn_PSD=warn_PSD, ret_B_err=ret_B_err)
+                         warn_PSD=warn_PSD, ret_B_err=ret_B_err,
+                         silent=True)
         Y_hat = np.dot(B_hat, Z)
         err = (np.linalg.norm(Y - Y_hat, ord='fro')**2) / T
-        if err < err_min:
-            opt_params = {'lmbda': l, 'alpha': a, 'delta': d, 'sigma': s}
-            B_opt = B_hat
-    return B_opt, opt_params
+        G = np.abs(sum([B_hat[:, tau * n:(tau + 1) * n]
+                        for tau in range(p)]).T)
+        G = G - np.diag(np.diag(G))
+        G = G > 0  # The Granger-causality graph
+        print('err = {:15.2f}'.format(err),
+              '(l = %9.4f, a = %6.5f, d = %6.5f, s = %9.4f) ' % tuple(x),
+              'Num edges: {:9d}'.format(np.sum(G)), end='\r')
+        return err
+
+    Y, Z = build_YZ(X_test, p)
+    T = Y.shape[1]
+    n = Y.shape[0]
+    bounds = [(0, 2), (0, 1), (0, 1), (0, 100)]  # l a d s
+    res = differential_evolution(f, bounds, disp=True, polish=False,
+                                 maxiter=100, popsize=25,
+                                 callback=tlimit_func, tol=1e-4)
+
+    print()  # Newline
+    print('Optimizer Success:', res.success)
+    l, a, d, s = res.x
+    print('Optimal parameters: lmbda = %0.5f, alpha = %0.5f, delta = %0.5f,'
+          ' sigma = %0.5f' % (l, a, d, s))
+    B_hat = dwglasso(ZZT=ZZT, YZT=YZT, p=p, lmbda=l, alpha=a,
+                     mu=mu, delta=d, sigma=s, tol=tol,
+                     warn_PSD=warn_PSD, ret_B_err=ret_B_err)
+    print()
+    plt.imshow(B_hat)
+    plt.colorbar()
+    plt.title('Optimal B_hat')
+    plt.show()
+    return B_hat
 
 
 # This function is ridiculously complicated
 def dwglasso(ZZT: np.array, YZT: np.array, p: int=1, lmbda: float=0.0,
              alpha: float=0.05, mu: float=0.1, delta=0, sigma=0,
-             tol=1e-6, max_iter=100, warn_PSD=True, ret_B_err=False):
+             tol=1e-6, max_iter=100, warn_PSD=True, ret_B_err=False,
+             silent=False, assert_params=True):
     '''Minimizes over B:
 
     1/(2T)||Y - BZ||_F^2 + lmbda[alpha||B||_F^2 + (1 - alpha)G_DW(B)
@@ -93,11 +127,12 @@ def dwglasso(ZZT: np.array, YZT: np.array, p: int=1, lmbda: float=0.0,
     then we have tikhonov regularization.  mu is a tuning parameter
     for ADMM convergence and unless lmbda = 0 or alpha = 1, we need mu > 0.
     '''
-    assert alpha >= 0 and alpha <= 1, 'Required: alpha \in [0, 1]'
-    assert lmbda >= 0, 'Required: lmbda >= 0'
-    assert mu >= 0, 'Required: mu >= 0'  # 0 only if lmbda = 0 or alpha = 1
-    assert sigma >= 0, 'Required: sigma >= 0'
-    assert delta >= 0 and delta <= 1, 'Required: delta \in [0, 1]'
+    if assert_params:
+        assert alpha >= 0 and alpha <= 1, 'Required: alpha \in [0, 1]'
+        assert lmbda >= 0, 'Required: lmbda >= 0'
+        assert mu >= 0, 'Required: mu >= 0'  # 0 only if lmbda = 0 or alpha = 1
+        assert sigma >= 0, 'Required: sigma >= 0'
+        assert delta >= 0 and delta <= 1, 'Required: delta \in [0, 1]'
 
     # Proximity operators
     # @numba.jit(nopython=True, cache=True)
@@ -153,17 +188,20 @@ def dwglasso(ZZT: np.array, YZT: np.array, p: int=1, lmbda: float=0.0,
         rel_err_k = rel_err(Bx, Bz)
         while rel_err_k > tol and k < max_iter:  # ADMM iterations
             k += 1
-            print('iter:', k, '(1/pn^2)||Bx - Bz||_F^2 =',
-                  rel_err_k, end='\r')
+            if not silent:
+                print('iter:', k, '(1/pn^2)||Bx - Bz||_F^2 =',
+                      rel_err_k, end='\r')
             sys.stdout.flush()
             Bx = proxf(Bz - Bu)
             Bz = proxg(Bx + Bu)
             Bu = Bu + Bx - Bz
             rel_err_k = rel_err(Bx, Bz)
-        print()  # Print out a newline
+        if not silent:
+            print()  # Print out a newline
         if k >= max_iter:  # Should only ever reach k == max_iter
-            warnings.warn('Max iterations exceeded! rel_err = %e'
-                          % rel_err_k, RuntimeWarning)
+            if not silent:
+                warnings.warn('Max iterations exceeded! rel_err = %e'
+                              % rel_err_k, RuntimeWarning)
         return Bz
 
     # ----------------REAL FUNCTION ENTRY POINT-----------------------
@@ -175,7 +213,7 @@ def dwglasso(ZZT: np.array, YZT: np.array, p: int=1, lmbda: float=0.0,
         sigma * np.eye(n * p)
 
     B_err = np.ones((n * p, n * p))
-    if warn_PSD:  # Check if ZZT > 0
+    if warn_PSD and not silent:  # Check if ZZT > 0
         try:
             cho_factor(ZZT)
         except np.linalg.LinAlgError as e:
@@ -187,7 +225,8 @@ def dwglasso(ZZT: np.array, YZT: np.array, p: int=1, lmbda: float=0.0,
                 return B_err
 
     if lmbda == 0:  # OLS
-        print('OLS')
+        if not silent:
+            print('OLS')
         try:
             L_and_lower = cho_factor(ZZT.T, overwrite_a=True)
             B = cho_solve(L_and_lower, YZT.T, overwrite_b=True,
@@ -201,7 +240,8 @@ def dwglasso(ZZT: np.array, YZT: np.array, p: int=1, lmbda: float=0.0,
 
     else:  # Regularized solution
         if alpha == 1:  # Tikhonov regularization with lmbda
-            print('L2 Regularization')
+            if not silent:
+                print('L2 Regularization')
             # R and subsequently YZT are overwritten in the lu solver
             R = (ZZT + lmbda * np.eye(n * p))  # Has nothing to do with Rx
             try:
@@ -216,11 +256,13 @@ def dwglasso(ZZT: np.array, YZT: np.array, p: int=1, lmbda: float=0.0,
                 return B
 
         else:  # DWGLASSO
-            print('DWGLASSO')
+            if not silent:
+                print('DWGLASSO')
             assert mu > 0, 'Required: mu > 0 (unless lmbda = 0, or alpha = 1)'
             if alpha == 0:
-                warnings.warn('We need alpha > 0 to guarantee convergence, '
-                              'to the optimal B matrix', RuntimeWarning)
+                if not silent:
+                    warnings.warn('We need alpha > 0 to guarantee convergence,'
+                                  ' to the optimal B matrix', RuntimeWarning)
             r = (1 + 2 * mu * lmbda * alpha) / mu
             R = (ZZT + r * np.eye(n * p))  # Has nothing to do with Rx
             try:
@@ -234,10 +276,6 @@ def dwglasso(ZZT: np.array, YZT: np.array, p: int=1, lmbda: float=0.0,
 
 
 def main():
-    import matplotlib as mpl
-    mpl.use('TkAgg')
-    from matplotlib import pyplot as plt
-
     p = 2
     assert p <= MAX_P and p >= 1, 'p must be in (1, MAX_P)!'
 
@@ -247,7 +285,7 @@ def main():
 
     Y_test, Z_test = build_YZ(XT, p)
 
-    B_hat = dwglasso(ZZT, YZT, p, lmbda=70.0, alpha=0.2, tol=1e-11,
+    B_hat = dwglasso(ZZT, YZT, p, lmbda=370.0, alpha=0.2, tol=1e-11,
                      mu=0.1, max_iter=150, sigma=2.5, delta=0.1,
                      ret_B_err=False)
     print('Non 0 entries:', np.sum(np.abs(B_hat) > 0),
