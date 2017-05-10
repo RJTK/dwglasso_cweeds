6c64cccbd2b8f78067062c7243b0daa590ff5118
Ryan J. Kinnear <Ryan@Kinnear.ca>
2017-05-10 14:18:42 -0400

    Lots of experimenting with the resulting plots

--- src/models/plot_results.py
+++ src/models/plot_results.py
@@ -5,7 +5,7 @@ NOTE: This file is intended to be executed by make from the top
 level of the project directory hierarchy.  We rely on os.getcwd()
 and it will not work if run directly as a script from this directory.
 '''
-from dwglasso import dwglasso
+from dwglasso import cross_validate, dwglasso
 import matplotlib as mpl; mpl.use('TkAgg')
 from matplotlib import pyplot as plt
 from itertools import combinations_with_replacement
@@ -15,7 +15,7 @@ import geopandas as gpd
 from shapely.geometry import Point, LineString
 
 from src.conf import CANADA_SHAPE, HDF_FINAL_FILE, LOCATIONS_KEY, MAX_P,\
-    P_LAG, ZZT_FILE_PREFIX, YZT_FILE_PREFIX
+    P_LAG, ZZT_FILE_PREFIX, YZT_FILE_PREFIX, X_VALIDATE_FILE_PREFIX
 
 
 def main():
@@ -23,15 +23,53 @@ def main():
     assert p <= MAX_P and p >= 1, 'p must be in (1, MAX_P)!'
     ZZT = np.load(ZZT_FILE_PREFIX + str(p) + '_T' + '.npy')
     YZT = np.load(YZT_FILE_PREFIX + str(p) + '_T' + '.npy')
+    X_test = np.load(X_VALIDATE_FILE_PREFIX + '_dT' + '.npy')
 
     # plt.imshow(ZZT)
     # plt.colorbar()
+    # plt.title('Covariance Matrix ZZT')
     # plt.show()
 
-    # Run dwglasso
-    B_hat = dwglasso(ZZT, YZT, p, lmbda=261.225, alpha=0.25, tol=1e-11,
-                     mu=0.01, max_iter=200, sigma=2.5, delta=0.1,
-                     ret_B_err=False)
+    # plt.imshow(YZT)
+    # plt.colorbar()
+    # plt.title('Cross-Covariance Matrix YZT')
+    # plt.show()
+
+    # plt.plot(np.sort(np.linalg.eigvals(ZZT)))
+    # plt.title('ZZT Eigenvalues')
+    # plt.show()
+
+    # # Run dwglasso
+    # # B_hat = cross_validate(ZZT, YZT, X_test, p, tol=1e-9)
+
+    # N_edges = []
+    # n = YZT.shape[0]
+    # Lmbda = np.linspace(30, 150, 350)
+    # G_total = np.zeros((n, n))
+    # for lmbda in Lmbda:
+    #     B_hat = dwglasso(ZZT, YZT, p, lmbda=lmbda, alpha=0.1,
+    #                      delta=0.1, sigma=15, silent=True, max_iter=250)
+    #     print('lmbda =%9.3f' % lmbda, end='\r')
+    #     G = np.abs(sum([B_hat[:, tau * n:(tau + 1) * n]
+    #                     for tau in range(p)]).T)
+    #     G = G > 0  # The Granger-causality graph
+    #     G = G - np.diag(np.diag(G))  # Zero the diagonal (no looping edges)
+    #     G_total += G
+    #     N_edges.append(np.sum(G))
+    # plt.plot(Lmbda, N_edges)
+    # plt.xscale('log')
+    # plt.title('N_edges vs lmbda (s = 15, alpha = 0.1, delta = 0.1, mu = 0.1')
+    # plt.xlabel('lmbda')
+    # plt.show()
+
+    # plt.imshow(G_total)
+    # plt.title('Edge count over lmbda')
+    # plt.colorbar()
+    # plt.show()
+    # return
+    # l, a, d, s = 0.00803, 0.80667, 0.77534, 99.76833  # on _dT
+    B_hat = dwglasso(ZZT, YZT, p, lmbda=1.0, alpha=0.1, delta=0.1,
+                     sigma=15.0, max_iter=250, mu=0.01, tol=1e-9)
 
     # plt.imshow(B_hat)
     # plt.colorbar()
@@ -44,7 +82,7 @@ def main():
     # Get the actual causality matrix
     G = np.abs(sum([B_hat[:, tau * n:(tau + 1) * n] for tau in range(p)]).T)
     G = G > 0  # The Granger-causality graph
-    G = G - np.diag(np.diag(G))  # Zero the diagonal (no looping edges)
+    G = G ^ np.diag(np.diag(G))  # Zero the diagonal (no looping edges)
 
     print('Num edges: ', np.sum(G), '/', ((n * p) * (n * p) + 1) // 2)
 
@@ -59,7 +97,7 @@ def main():
     ax.set_title('Canada')
     ax.axes.get_yaxis().set_ticks([])  # Remove the ticks
     ax.axes.get_xaxis().set_ticks([])
-    canada.plot(ax=ax, color='white')
+    canada.plot(ax=ax, color='white', linewidth=0.5)
 
     hdf_final = pd.HDFStore(HDF_FINAL_FILE, mode='r')
     D_loc = hdf_final[LOCATIONS_KEY]
@@ -87,10 +125,12 @@ def main():
                               geometry=arrow_geometry,
                               crs={'init': 'epsg:4326'})
     arrows.to_crs(crs=canada.crs, inplace=True)
-    arrows.plot(ax=ax, color='black')
+    arrows.plot(ax=ax, color='green', linewidth=1.5)
     plt.show()
     return
 
 
 if __name__ == '__main__':
+    # l, a, d, s = 5.34048, 0.90798, 0.09682, 14.44046  # on _T
+    # l, a, d, s = 0.00803, 0.80667, 0.77534, 99.76833  # on _dT
     main()
